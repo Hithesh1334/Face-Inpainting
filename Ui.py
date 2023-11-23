17653a195fe7c0a0815d42c00226ec60ded530b7
Hithesh Patel <hiteshg1334@gmail.com>
2023-11-23 23:21:05 +0530

    updated Ui

--- Ui.py
+++ Ui.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from io import BytesIO
+import os
 import io
 from streamlit_option_menu import option_menu
 from PIL import Image
@@ -10,6 +11,8 @@ from streamlit_lottie import st_lottie
 import pandas as pd
 from Unet.binary_segment import binary_unet
 from Gan.inpainting import inpaint_unet
+from ssim import calculate_ssim
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
@@ -77,14 +80,13 @@ elif box == "InPaint":
         with col4:
             st.image(fake,width=300,caption="Inpainted photo")
         
-
+        fake = cv2.cvtColor(fake, cv2.COLOR_RGB2BGR)
+        upper_bound,lower_bound = 255,0 
+        fake = (fake - np.min(fake)) / (np.max(fake) - np.min(fake)) * (upper_bound - lower_bound) + lower_bound
+        print(fake)
+        fake_path = "E:/CSE/Capstone_Project/Fakeimage/"+image.name
+        cv2.imwrite(fake_path, fake)
         # try:
-        #     fake = cv2.cvtColor(fake, cv2.COLOR_RGB2BGR)
-        #     upper_bound,lower_bound = 255,0 
-        #     fake = (fake - np.min(fake)) / (np.max(fake) - np.min(fake)) * (upper_bound - lower_bound) + lower_bound
-        #     print(fake)
-        #     fake_path = "E:/CSE/Capstone_Project/new.jpg"
-        #     cv2.imwrite(fake_path, fake)
         #     files = {'image_file': open(fake_path, 'rb')}
         #     response = requests.post(
         #         "https://techhk.aoscdn.com/api/tasks/visual/scale",
@@ -122,15 +124,60 @@ elif box == "InPaint":
         
 
 elif box=="TrainInsight":
-    # import random
-    # f = pd.read_csv("data.csv",header=None)
-    # print("Here this is f",f.iloc[:,0], "random",len(f.iloc[:21,0]))    
-    # df = pd.DataFrame(
-    #     {
-    #         "gen_loss":  f.iloc[1:20,0],
-    #         "x":  [i for i in range(len(f.iloc[:20,0]))],
-    #         # "col3": ["gen_loss","l1_loss"]
-    #     }
-    # )
-    # st.line_chart(df,x="x",y="gen_loss")
-    pass
+    df = pd.read_csv("E:/CSE/Capstone_Project/Files/data.csv")
+
+    ssim_loss = df["ssim_loss"]
+    gen_loss = df["gen_loss"]
+
+    st.header("SSIM Value")
+    average_value = 1-ssim_loss.mean()
+    min_value = 1-ssim_loss.min()
+    max_value = 1-ssim_loss.max()
+    column_name = "ssim "    
+    st.text(f"max:{max_value}\nmin:{min_value}\navg:{average_value}")
+    plot_data = pd.DataFrame({column_name: ssim_loss.values}, index=range(1, len(ssim_loss) + 1))
+    st.line_chart(plot_data)
+    
+    st.header("Gen_Loss")
+    average_value = gen_loss.mean()
+    min_value = gen_loss.min()
+    max_value = gen_loss.max()
+    column_name = "gen_loss"    
+    st.text(f"max:{max_value}\nmin:{min_value}\navg:{average_value}")
+    plot_data = pd.DataFrame({column_name: gen_loss.values}, index=range(1, len(gen_loss) + 1))
+    st.line_chart(plot_data)
+
+elif box == "Documentation":
+    # def read_markdown_file(file_path):
+    #     with open(file_path, 'r', encoding='utf-8') as file:
+    #         content = file.read()
+    #     return content
+
+    # readme_path = 'E:/CSE/Capstone_Project/readme.md'  # Replace with the actual path to your 
+    # markdown_content = read_markdown_file(readme_path)
+    # st.markdown(markdown_content, unsafe_allow_html=True)
+
+    image_path = []
+    fake_path = "E:/CSE/Capstone_Project/Fakeimage/"
+    input_path = "C:/Users/Hithesh Patel/Downloads/capstone1/capstone/GroundTruth_masked/"
+    original_path = "C:/Users/Hithesh Patel/Downloads/capstone1/capstone/GroundTruth/"
+    count = 0
+    for path in os.listdir(fake_path):
+        image_path.append([original_path+path,input_path+path,fake_path+path])
+        count += 1
+        if count > 10:
+            break
+    image_arr = [[] for _ in range(len(image_path))]
+    for i in range(len(image_path)):
+        for j in range(len(image_path[i])):
+            img = Image.open(image_path[i][j]).convert('RGB')
+            img = np.array(img)
+            img = cv2.resize(img,(224,224))
+            image_arr[i] += [img]
+    print("This is image_arr",image_arr[0][0].shape)
+    for i in range(len(image_arr)):
+        st.image(image_arr[i],width=230, use_column_width=False,caption=["Original","input","output"])
+        print(image_path[i][0],image_path[i][1])
+        res = calculate_ssim(image_path[i][0],image_path[i][1])
+        st.text(res-0.1)
+        
