edd35c758d0318e8b79da4c0257cc74e1c35384c
Hithesh Patel <hiteshg1334@gmail.com>
2023-11-28 17:25:02 +0530

    updated renamefile

--- renameFiles.py
+++ renameFiles.py
@@ -1,15 +1,16 @@
 import os
 
 #Code to rename the files accordingly
-path = "E:/CSE/finalDataset/mergedDataset"
-s = os.listdir(path)
-print(len(s))
-# count = 1
-# for i in os.listdir(path):
-#     innerPath = path + i + "/"
-#     for j in os.listdir(innerPath):
-#         oldFileName = innerPath + j
-#         newFileName = path + "mergedDataset/" + "00" + str(count) +".jpg"
-#         count += 1
-#         os.rename(oldFileName, newFileName)
+path = "E:/CSE/finalDataset/"
+# s = os.listdir(path)
+# print(len(s))
+count = 1
+for i in os.listdir(path):
+    innerPath = path + i + "/"
+    for j in os.listdir(innerPath):
+        oldFileName = innerPath + j
+        newFileName = path + "mergedDataset/" + "00" + str(count) +".jpg"
+        count += 1
+        os.rename(oldFileName, newFileName)
+
 
